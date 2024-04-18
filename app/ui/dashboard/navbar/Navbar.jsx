87d760da2049cfe78bf01853dca80bc9c641351d
devhumayun <humayunkabir9408@gmail.com>
2024-04-18 21:36:40 +0600

    navbar component design complate

--- app/ui/dashboard/navbar/Navbar.jsx
+++ app/ui/dashboard/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
+"use client"
 
-// import styles from './navbar.module.css'
+import { usePathname } from "next/navigation";
+import {
+    MdNotifications,
+    MdOutlineChat,
+    MdPublic,
+    MdSearch,
+} from "react-icons/md";
+import styles from './navbar.module.css';
 
 const Navbar = () => {
+    const pathname = usePathname()
     return (
-        <div>
-            Navbar
+        <div className={styles.container}>
+            <div className={styles.title}>{pathname.split("/").pop()}</div>
+            <div className={styles.menu}>
+                <div className={styles.search}>
+                    <MdSearch className={styles.searchIcon} />
+                    <input type="text" placeholder="Search..." className={styles.input} />
+                </div>
+                <div className={styles.icons}>
+                    <MdOutlineChat size={20} />
+                    <MdNotifications size={20} />
+                    <MdPublic size={20} />
+                </div>
+            </div>
         </div>
     )
 }
