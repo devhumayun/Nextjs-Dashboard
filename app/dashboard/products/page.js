9ce46763e3a320c7b8a22c9304f05b3e68170bb0
devhumayun <humayunkabir9408@gmail.com>
2024-04-19 08:20:02 +0600

    refactor

--- app/dashboard/products/page.js
+++ app/dashboard/products/page.js
@@ -1,7 +1,67 @@
-import Product from "@/app/ui/product/Product";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineDelete } from "react-icons/ai";
+import { GoEye } from "react-icons/go";
+
+import Pagination from "@/app/ui/pagination/Pagination";
+import styles from "@/app/ui/product/product.module.css";
+import Search from "@/app/ui/search/Search";
+import Button from "@/app/ui/user/Button";
 
 const ProductPage = () => {
-  return <Product />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.topbar}>
+        <Search placeholder="Search products..." />
+        <Button />
+      </div>
+      <table className={styles.table}>
+        <thead className={styles.tableheading}>
+          <tr>
+            <th>Title</th>
+            <th>Descripation</th>
+            <th>Price</th>
+            <th>Created At</th>
+            <th>Stock</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tablebody}>
+          <tr>
+            <td>
+              <div className={styles.product_iname}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                <span className={styles.productTitle}>Redmi</span>
+              </div>
+            </td>
+            <td>
+              <span>Lorem</span>
+            </td>
+            <td>$200</td>
+            <td>1 day</td>
+            <td>12</td>
+            <td>
+              <div className={styles.actionBtn}>
+                <Link href={"#"} className={styles.viewBtn}>
+                  <GoEye />
+                </Link>
+                <button className={styles.deleteBtn}>
+                  <AiOutlineDelete />
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductPage;
--- app/dashboard/users/page.js
+++ app/dashboard/users/page.js
@@ -1,9 +1,66 @@
-import User from "@/app/ui/user/User";
+import Pagination from "@/app/ui/pagination/Pagination";
+import Search from "@/app/ui/search/Search";
+import Button from "@/app/ui/user/Button";
+import styles from "@/app/ui/user/user.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineDelete } from "react-icons/ai";
+import { GoEye } from "react-icons/go";
 
 const UserPage = () => {
   return (
-    <div>
-      <User />
+    <div className={styles.container}>
+      <div className={styles.topbar}>
+        <Search placeholder="Search users..." />
+        <Button />
+      </div>
+      <table className={styles.table}>
+        <thead className={styles.tableheading}>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created At</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tablebody}>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                humayun@gmail.com
+              </span>
+            </td>
+            <td>1 hour</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.actionBtn}>
+                <Link href={"#"} className={styles.viewBtn}>
+                  <GoEye />
+                </Link>
+                <button className={styles.deleteBtn}>
+                  <AiOutlineDelete />
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
     </div>
   );
 };
